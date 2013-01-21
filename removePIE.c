62b3621b6c2ef474a34dcdcfb2d4e5287d52e922
Peter Fillmore <peter@peterfillmore.com>
2013-01-21 18:15:38 +1100

    Updated executable so it backs up the original binary

--- removePIE.c
+++ removePIE.c
@@ -4,48 +4,43 @@
 #include <stdint.h>
 #include <mach-o/loader.h>
 
-void hexify(unsigned char *data, uint32_t size)
-{
+void hexify(unsigned char *data, uint32_t size){
 	while(size--)
-		printf("%02x", *data++);
+		printf("%02x", *data++);}
+
+void fcopy(FILE *f1, FILE *f2){
+    char            buffer[BUFSIZ];
+    size_t          n;
+
+    while ((n = fread(buffer, sizeof(char), sizeof(buffer), f1)) > 0){
+        if (fwrite(buffer, sizeof(char), n, f2) != n)
+            printf("Error copying backup");}
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
 	struct mach_header currentHeader;
 	//int32_t magic = 0;
-	FILE *fp;
+	FILE *fp; //edited file pointer
+	FILE *fw; //backup file pointer
+	char fwName[80];
+	char fwPrefix[4] = ".bak"; //app.bak
 	
-	if(argc < 1)
-	{	
+	if(argc < 1){	
 		printf("Please enter the filename binary: in the format removePIE filename");
-		return EXIT_FAILURE;
-	}
-	
-	if((fp = fopen(argv[1], "rb+")) == NULL) 
-	{
+		return EXIT_FAILURE;}
+	if((fp = fopen(argv[1], "rb+")) == NULL) {
 		printf("Error, unable to open file\n");
-		return EXIT_FAILURE; 
-	}
+		return EXIT_FAILURE; }
+	//create app.bak filename
+	strlcpy(fwName, argv[1], strlen(argv[1])+1);
+	strlcat(fwName, fwPrefix, strlen(fwPrefix)+1);
+	if((fw = fopen(fwName, "wb")) == NULL){
+	return EXIT_FAILURE;}
+	
 	if((fread(&currentHeader.magic, sizeof(int32_t), 1, fp)) == (int)NULL)
-	{
-		printf("Error reading magic constant in file\n");
-		return EXIT_FAILURE;
-	}
-	if(currentHeader.magic == MH_MAGIC) //little endian
-	{
-		//extract the entitlements file
-		//if((execve("/usr/bin/ldid", 
-		/*
-		int32_t cputype = 0;
-		int32_t cpusubtype = 0;
-		int32_t filetype = 0;
-		int32_t ncmds = 0;
-		int32_t sizeofcmds = 0;
-		int32_t flags = 0;
-		*/
-		//char *mach_header = (char *) alloca(28 * sizeof(char));
-
+	{printf("Error reading magic constant in file\n");
+		return EXIT_FAILURE;}
+	if(currentHeader.magic == MH_MAGIC){ //little endian
 		printf("loading header\n");
 		fseek(fp, 0, SEEK_SET);
 		if((fread(&currentHeader, sizeof(currentHeader), 1, fp)) == (int)NULL)
@@ -53,6 +48,10 @@ int main(int argc, char *argv[])
 			printf("Error reading MACH-O header");
 			return EXIT_FAILURE;
 		}	
+		fseek(fp, 0, SEEK_SET); //set fp back to 0 to get full copy
+		printf("backing up application binary...\n");
+		fcopy(fp, fw);
+		fclose(fw);
 		printf("mach_header:");
 		hexify((unsigned char *)&currentHeader,sizeof(currentHeader));	
 		printf("\nDisabling ASLR/PIE\n");
